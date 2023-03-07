1ef0e6fcabfe25c76d9aa78dda9c00bf0c86732b
SofiaLazetera <sofia.lazetera@gds.ey.com>
2023-03-07 20:13:31 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form ></form>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = () => {
+
+
+    return
+    <>
+        <div>Hola soy una card</div>
+    </>
+};
+
+export default Card
