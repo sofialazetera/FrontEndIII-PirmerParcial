4b90b4316d2249aab4c4013b93e85cfb92465ddf
SofiaLazetera <sofia.lazetera@gds.ey.com>
2023-03-08 18:58:16 -0300

    Test commit before exam

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  //test return
   return (
     <div className="App">
       <h1>Carga de estudiantes</h1>
       <form >
+      <input
+        type="text"
+        placeholder="Nombre de usuario"
         
+      />
       </form>
       <Card />
     </div>
